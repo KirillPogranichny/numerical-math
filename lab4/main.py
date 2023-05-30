da9aae250b53c2541d1f42bf5cf23e1188fc5d52
KirillPogranichny <kirillpogranichny@gmail.com>
2023-05-30 21:03:04 +1000

    lab4 solve

---
+++ lab4/main.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+
+def proj(x, y):
+    return y * (x.dot(y) / y.dot(y))
+
+
+def qr_decomposition(A):
+    dim = A.shape
+    R = np.zeros((dim[1], dim[1]))
+    U = np.zeros(dim)
+    C = np.zeros(dim)
+
+    for k in range(dim[0]):
+        sum = 0
+
+        if k > 0:
+            for j in range(k-1):
+                sum += proj(A.transpose()[k], U[j])
+
+        U[k] = A.transpose()[k] - sum
+        C[k] = U[k] / np.linalg.norm(U[k])
+
+    for i in range(dim[1]):
+        for j in range(dim[1]):
+            if i <= j:
+                R[i][j] = C[i].dot(A.transpose()[j])
+
+    Q = C.T
+
+    return Q, R, U
+
+
+def get_solution(A, b):
+    Q, R, U = qr_decomposition(A)
+    y = np.linalg.tensorsolve(Q, b)
+
+    return np.linalg.tensorsolve(U, y)
+
+
+if __name__ == "__main__":
+    A = np.array([
+        [2, 0, 1],
+        [0, 1, -1],
+        [1, 1, 1]
+    ])
+    b = np.array([3, 0, 3])
+
+    Q, R, U = qr_decomposition(A)
+    print("Решение системы:\n", get_solution(A, b))
